611bb0899e2caf288a56d3da8a9f971493e1dc5f
LiliwoL Dell <github@liliwol.fr>
2022-03-14 15:57:47 +0100

    Tests unitaires des entités

--- README.md
+++ README.md
@@ -60,3 +60,167 @@ Dans le cas où l'autoload e composer n'est pas correct, vous pouvez faire un **
 
 ***
 
+## PhpUnit
+
+
+On va utiliser PhpUnit pour faire les tests
+
+### Installation
+
+    composer require phpunit/phpunit --dev
+
+La dépendance est installée en mode **dev**
+
+
+### Ecriture du code
+
+Dans le dossier **<root>/tests/Entite**, créez le fichier **PanietTest.php**
+
+```php
+<?php
+
+namespace App\Tests\Entite;
+
+use App\Entite\Collection;
+use App\Entite\Ligne;
+use App\Entite\Panier;
+use App\Entite\Ramette;
+use App\Entite\Stylo;
+use PHPUnit\Framework\TestCase;
+
+class PanierTest extends TestCase
+{
+    private Collection $articles;
+
+    // Méthode appelée au lancement de ce test
+    public function setUp(): void
+    {
+        $this->articles = new Collection();
+        $unBic = new Stylo("Bic", "BBOrange", "Bic bille Orange", 1.2, 20, "Bleu");
+        $uneRamette = new Ramette("Clairef", "CRA4S", "Ramette A4 Sup", 9, 20, 80);
+        
+        $this->articles->append($unBic);
+        $this->articles->append($uneRamette);
+        
+        $this->articles->append(new Stylo("Stypen", "PlumeS", "Stylo Plume Stypen", 5.5, 20, "jaune"));
+        $this->articles->append(new Stylo("Waterman", "WOBGreen", "Waterman Orion Bille vert", 5.5, 20, "vert"));
+        $this->articles->append(new Stylo("Parker", "PlumeP", "Stylo Plume Parker", 5.5, 5, "noir"));
+    }
+
+    // Méthode pour vérifier le montant du panier
+    public function testJePeuxAjouterUneLigneAMonPanier()
+    {
+        $panier = new Panier();
+        $ligne = new Ligne($this->articles->offsetGet(1), 2);
+        $panier->addLigne($ligne);
+
+        $montant = 0;
+        foreach($panier->getLignes() as $ligne) 
+        {
+            $montant += $ligne->getArticle()->getPrixUnitaire()*$ligne->getQte();
+        }
+
+        $panier->setMontant($montant);
+
+        $this->assertEquals(18, $panier->getMontant());
+    }
+
+    public function testJePeuxAjouterPlusieursLignesAMonPanier()
+    {
+        $panier = new Panier();
+
+        // Ajout de 4 stylo à 1.20€ pièce puis
+        $ligne = new Ligne($this->articles->offsetGet(0), 4);
+        $panier->addLigne($ligne);
+
+        // Ajout de 2 ramettes à 9€ pièce
+        $ligne = new Ligne($this->articles->offsetGet(1), 2);
+        $panier->addLigne($ligne);
+
+        $montant = 0;
+        foreach($panier->getLignes() as $ligne) 
+        {
+            $montant += $ligne->getArticle()->getPrixUnitaire()*$ligne->getQte();
+        }
+
+        $panier->setMontant($montant);
+
+        $this->assertEquals(22.8, $panier->getMontant());
+    }
+
+    public function testJePeuxRetirerUnArticleDeMonPanier()
+    {
+        $panier = new Panier();
+
+        // Ajout de 4 stylo à 1.20€ pièce puis
+        $ligne1 = new Ligne($this->articles->offsetGet(0), 4);
+        $panier->addLigne($ligne1);
+
+        // Ajout de 2 ramettes à 9€ pièce
+        $ligne2 = new Ligne($this->articles->offsetGet(1), 2);
+        $panier->addLigne($ligne2);
+
+        $panier->removeLigne($ligne1);
+
+        $montant = 0;
+        foreach($panier->getLignes() as $ligne) 
+        {
+            $montant += $ligne->getArticle()->getPrixUnitaire()*$ligne->getQte();
+        }
+
+        $panier->setMontant($montant);
+
+        $this->assertEquals(18, $panier->getMontant());
+    }
+
+    public function testJePeuxModifierLaQuantiteDUnArticleDeMonPanier()
+    {
+        $panier = new Panier();
+
+        // Ajout de 4 stylo à 1.20€ pièce puis
+        $ligne1 = new Ligne($this->articles->offsetGet(0), 4);
+        $panier->addLigne($ligne1);
+
+        // Ajout de 2 ramettes à 9€ pièce
+        $ligne2 = new Ligne($this->articles->offsetGet(1), 2);
+        $panier->addLigne($ligne2);
+
+        $montant = 0;
+        foreach($panier->getLignes() as $ligne) 
+        {
+            $montant += $ligne->getArticle()->getPrixUnitaire()*$ligne->getQte();
+        }
+
+        $panier->setMontant($montant);
+
+        $this->assertEquals(22.8, $panier->getMontant());
+
+        $ligneStylo = $panier->getLignes()->offsetGet(0);
+        $ligneStylo->setQte($ligneStylo->getQte() + 1);
+
+        $montant = 0;
+        foreach($panier->getLignes() as $ligne) 
+        {
+            $montant += $ligne->getArticle()->getPrixUnitaire()*$ligne->getQte();
+        }
+
+        $panier->setMontant($montant);
+
+        $this->assertEquals(24, $panier->getMontant());
+    }
+}
+```
+
+### Lancement des tests
+
+Avec la commande suivante, les tests placés dans le fichier précédent vont être lancés:
+
+    $ php vendor/bin/phpunit ./tests/Entite/PanierTest.php
+    PHPUnit 9.5.18 #StandWithUkraine
+
+    ....                                                                4 / 4 (100%)
+
+    Time: 00:00.018, Memory: 4.00 MB
+
+    OK (4 tests, 5 assertions)
+
--- tests/Entite/PanierTest.php
+++ tests/Entite/PanierTest.php
@@ -3,29 +3,21 @@
 namespace App\Tests\Entite;
 
 use App\Entite\Collection;
-use App\Entite\Stylo;
-use App\Entite\Ramette;
-use App\Entite\Panier;
 use App\Entite\Ligne;
-
+use App\Entite\Panier;
+use App\Entite\Ramette;
+use App\Entite\Stylo;
 use PHPUnit\Framework\TestCase;
 
 class PanierTest extends TestCase
 {
-    // Attribut
     private Collection $articles;
 
-    // Premier Test de mise en place d'un panier
-    // et l'ajout d'élément sà l'intérieur
-    public function testSetUp(): void
+    // Méthode appelée au lancement de ce test
+    public function setUp(): void
     {
-        // Instance de Collection
         $this->articles = new Collection();
-
-        // Instance de Stylo
         $unBic = new Stylo("Bic", "BBOrange", "Bic bille Orange", 1.2, 20, "Bleu");
-
-        // Instance de Ramette
         $uneRamette = new Ramette("Clairef", "CRA4S", "Ramette A4 Sup", 9, 20, 80);
         
         $this->articles->append($unBic);
@@ -34,45 +26,107 @@ class PanierTest extends TestCase
         $this->articles->append(new Stylo("Stypen", "PlumeS", "Stylo Plume Stypen", 5.5, 20, "jaune"));
         $this->articles->append(new Stylo("Waterman", "WOBGreen", "Waterman Orion Bille vert", 5.5, 20, "vert"));
         $this->articles->append(new Stylo("Parker", "PlumeP", "Stylo Plume Parker", 5.5, 5, "noir"));
-
-        // Un vrai test pour vérifier le nombre d'éléments du panier
-        $this->assertEquals(count($this->articles), 5);
     }
 
-
-    // Tester l'ajout d'une ligne au panier
-    public function testJePeuxajouterUneLigneAMonPanier()
+    // Méthode pour vérifier le montant du panier
+    public function testJePeuxAjouterUneLigneAMonPanier()
     {
-        // Instance de Collection
-        $this->articles = new Collection();
-
-        // Instance de Stylo
-        $unBic = new Stylo("Bic", "BBOrange", "Bic bille Orange", 1.2, 20, "Bleu");
-        // Instance de Ramette
-        $uneRamette = new Ramette("Clairef", "CRA4S", "Ramette A4 Sup", 9, 20, 80);        
-        $this->articles->append($unBic);
-        $this->articles->append($uneRamette);        
-        $this->articles->append(new Stylo("Stypen", "PlumeS", "Stylo Plume Stypen", 5.5, 20, "jaune"));
-        $this->articles->append(new Stylo("Waterman", "WOBGreen", "Waterman Orion Bille vert", 5.5, 20, "vert"));
-        $this->articles->append(new Stylo("Parker", "PlumeP", "Stylo Plume Parker", 5.5, 5, "noir"));
-
-
-        // Instance de panier
         $panier = new Panier();
-        $ligne = new Ligne($this->articles->offsetGet(1), 2); // Récupération l'article  $uneRamette à 9 euros, on met 2 à quantité      
-        
-        // Ajout de cette ligne au panier
+        $ligne = new Ligne($this->articles->offsetGet(1), 2);
         $panier->addLigne($ligne);
 
-        // Calcul du montant du panier
         $montant = 0;
         foreach($panier->getLignes() as $ligne) 
         {
             $montant += $ligne->getArticle()->getPrixUnitaire()*$ligne->getQte();
         }
+
         $panier->setMontant($montant);
 
-        // Assertion d'égalité du montant du panier à 18 euros
-        $this->assertEquals($panier->getMontant(), 18);
+        $this->assertEquals(18, $panier->getMontant());
+    }
+
+    public function testJePeuxAjouterPlusieursLignesAMonPanier()
+    {
+        $panier = new Panier();
+
+        // Ajout de 4 stylo à 1.20€ pièce puis
+        $ligne = new Ligne($this->articles->offsetGet(0), 4);
+        $panier->addLigne($ligne);
+
+        // Ajout de 2 ramettes à 9€ pièce
+        $ligne = new Ligne($this->articles->offsetGet(1), 2);
+        $panier->addLigne($ligne);
+
+        $montant = 0;
+        foreach($panier->getLignes() as $ligne) 
+        {
+            $montant += $ligne->getArticle()->getPrixUnitaire()*$ligne->getQte();
+        }
+
+        $panier->setMontant($montant);
+
+        $this->assertEquals(22.8, $panier->getMontant());
+    }
+
+    public function testJePeuxRetirerUnArticleDeMonPanier()
+    {
+        $panier = new Panier();
+
+        // Ajout de 4 stylo à 1.20€ pièce puis
+        $ligne1 = new Ligne($this->articles->offsetGet(0), 4);
+        $panier->addLigne($ligne1);
+
+        // Ajout de 2 ramettes à 9€ pièce
+        $ligne2 = new Ligne($this->articles->offsetGet(1), 2);
+        $panier->addLigne($ligne2);
+
+        $panier->removeLigne($ligne1);
+
+        $montant = 0;
+        foreach($panier->getLignes() as $ligne) 
+        {
+            $montant += $ligne->getArticle()->getPrixUnitaire()*$ligne->getQte();
+        }
+
+        $panier->setMontant($montant);
+
+        $this->assertEquals(18, $panier->getMontant());
+    }
+
+    public function testJePeuxModifierLaQuantiteDUnArticleDeMonPanier()
+    {
+        $panier = new Panier();
+
+        // Ajout de 4 stylo à 1.20€ pièce puis
+        $ligne1 = new Ligne($this->articles->offsetGet(0), 4);
+        $panier->addLigne($ligne1);
+
+        // Ajout de 2 ramettes à 9€ pièce
+        $ligne2 = new Ligne($this->articles->offsetGet(1), 2);
+        $panier->addLigne($ligne2);
+
+        $montant = 0;
+        foreach($panier->getLignes() as $ligne) 
+        {
+            $montant += $ligne->getArticle()->getPrixUnitaire()*$ligne->getQte();
+        }
+
+        $panier->setMontant($montant);
+
+        $this->assertEquals(22.8, $panier->getMontant());
+
+        $ligneStylo = $panier->getLignes()->offsetGet(0);
+        $ligneStylo->setQte($ligneStylo->getQte() + 1);
+
+        $montant = 0;
+        foreach($panier->getLignes() as $ligne) 
+        {
+            $montant += $ligne->getArticle()->getPrixUnitaire()*$ligne->getQte();
+        }
+
+        $panier->setMontant($montant);
+
+        $this->assertEquals(24, $panier->getMontant());
     }
 }
