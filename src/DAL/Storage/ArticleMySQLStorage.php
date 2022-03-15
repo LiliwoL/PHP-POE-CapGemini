2d635f64864916c7cb6c23127f84eb8911b57027
LiliwoL <formation@liliwol>
2022-03-15 15:56:43 +0000

    TP2

--- src/DAL/Storage/ArticleMySQLStorage.php
+++ src/DAL/Storage/ArticleMySQLStorage.php
@@ -16,9 +16,15 @@ class ArticleMySQLStorage
 
     public function find(int $id): ?array
     {
-        $sth = $this->dbh->prepare('SELECT * FROM articles WHERE id_article = :id');
+        $sth = $this->dbh->prepare('
+            SELECT * 
+            FROM articles 
+            WHERE id_article = :id
+        ');
         $sth->execute([':id' => $id]);
+        
         $result = $sth->fetch(PDO::FETCH_ASSOC);
+
         if (!$result) {
             return null;
         }
@@ -28,7 +34,8 @@ class ArticleMySQLStorage
     public function insert(string $marque, string $reference, string $designiation, float $prixUnitaire, int $qteStock, string $type, ?string $couleur = null, ?int $grammage = null): int
     {
         $sth = $this->dbh->prepare('
-            INSERT INTO articles (reference, marque, designation, prix_unitaire, qte_stock, couleur, grammage, type)
+            INSERT INTO articles 
+            (reference, marque, designation, prix_unitaire, qte_stock, couleur, grammage, type)
             VALUES (:reference, :marque, :designation, :prix_unitaire, :qte_stock, :couleur, :grammage, :type)
         ');
 
@@ -43,6 +50,9 @@ class ArticleMySQLStorage
             ':type' => $type,
         ]);
 
+        // LastInsertId renvoie une string ou false
+        // https://www.php.net/manual/fr/pdo.lastinsertid.php
+        // Or, nous on veut un entier!
         return (int)$this->dbh->lastInsertId();
     }
 
@@ -50,7 +60,8 @@ class ArticleMySQLStorage
     {
         $sth = $this->dbh->prepare('
             UPDATE articles
-            SET reference = :reference, marque = :marque, designation = :designation, prix_unitaire = :prix_unitaire, qte_stock = :qte_stock, couleur = :couleur
+            SET reference = :reference, marque = :marque, designation = :designation,
+            prix_unitaire = :prix_unitaire, qte_stock = :qte_stock, couleur = :couleur
             WHERE id_article = :id_article
         ');
 
@@ -72,6 +83,8 @@ class ArticleMySQLStorage
             WHERE id_article = :id_article
         ');
 
+        // la méthode execute renvoie un boolean
+        // https://www.php.net/manual/fr/pdostatement.execute.php
         return (int) $sth->execute([
             ':id_article' => $id
         ]);
@@ -79,6 +92,7 @@ class ArticleMySQLStorage
 
     public function findAll(string $sortBy, string $direction = 'ASC'): array
     {
+        // On s'assure du champ utilisé pour le tri
         if (!in_array($sortBy, [
             'id_article',
             'reference',
@@ -93,21 +107,35 @@ class ArticleMySQLStorage
             throw new InvalidArgumentException('Invalid field name');
         }
 
+        // On s'assure qu'on a pas autre chose que ASC ou DESC
         if (!in_array($direction, [
             'ASC',
             'DESC',
         ], true)) {
             throw new InvalidArgumentException('Invalid ORDER BY direction');
         }
-        $sth = $this->dbh->prepare("SELECT * FROM articles ORDER BY $sortBy $direction");
+
+
+        $sth = $this->dbh->prepare("
+            SELECT * 
+            FROM articles 
+            ORDER BY $sortBy $direction
+        ");
         $sth->execute();
+
         return $sth->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function findAllByType(string $type): array
     {
-        $sth = $this->dbh->prepare('SELECT * FROM articles WHERE type = :type');
+        $sth = $this->dbh->prepare('
+            SELECT * 
+            FROM articles 
+            WHERE type = :type
+        ');
+
         $sth->execute([':type' => $type]);
+        
         return $sth->fetchAll(PDO::FETCH_ASSOC);
     }
 }
