7778e5ea4ffaa22180e65e9cf476e81ed05af586
LiliwoL <formation@liliwol>
2022-03-15 13:57:38 +0000

    TP2

--- script.sql
+++ script.sql
@@ -9,3 +9,16 @@ INSERT INTO `stagiaires` (`identifiant`, `nom`) VALUES
 (1,	'Inès'),
 (2,	'Marie'),
 (3,	'Yassine');
+
+
+CREATE TABLE articles (
+    id_article INT PRIMARY KEY AUTO_INCREMENT,
+    reference CHAR(10) NOT NULL,
+    marque VARCHAR(200) NOT NULL,
+    designation VARCHAR(200) NOT NULL,
+    prix_unitaire float NOT NULL,
+    qte_stock INT NOT NULL, 
+    grammage INT NULL,
+    couleur VARCHAR(50) NULL, 
+    type CHAR(10) NOT NULL
+);
